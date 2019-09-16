ee2587e5301e4e7d8ee54ae59766792f58ec074e
Gauthier Segay <gauthier.segay@gmail.com>
2019-09-17 00:20:05 +0200

    add byte type

--- tests/FSharp.UMX.Tests.fs
+++ tests/FSharp.UMX.Tests.fs
@@ -31,6 +31,16 @@ let ``Simple unit of measure conversions with cast operator``() =
     let y : string<skuId> = % (%x).ToString()
     let z : int<km> = % 42
     let w : string<foo> = % sprintf "%O %s %d" %x %y %z
+    let b : byte<foo> = % 1uy
+    ()
+
+[<Fact>]
+let ``Simple unit of measure conversions with UMX.tag function``() =
+    let x = UMX.tag<skuId> (Guid.NewGuid())              
+    let y = UMX.tag<skuId> ((%x).ToString())             
+    let z = UMX.tag<km> (42)                          
+    let w = UMX.tag<foo> (sprintf "%O %s %d" %x %y %z) 
+    let b = UMX.tag<foo> (1uy)                         
     ()
 
 [<Fact>]
